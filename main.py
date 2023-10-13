d2f3019aaaf6276b586ed00f1d86f426d3b175ec
khamovnik <147813836+khamovnik@users.noreply.github.com>
2023-10-13 13:13:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,10 @@ rows_2 = processed_text_2.split('\n')
 CreateCsv(csv_name_2, rows_2)
 print("Второй файл обработан!")
 
+pdf_name_3 = 'Vet.pdf'
+csv_name_3 = 'output_new3.csv'
+pdf_text_3 = ReadFile(pdf_name_3)
+processed_text_3 = ReplaceTermin(pdf_text_3, terminology_dict)
+rows_3 = processed_text_3.split('\n')
+CreateCsv(csv_name_3, rows_3)
+print("Третий файл обработан!")
