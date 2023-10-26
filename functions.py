03395646ff7c33535afaf145b21d56996d7fd44d
khamovnik <147813836+khamovnik@users.noreply.github.com>
2023-10-26 23:55:28 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -9,8 +9,8 @@ def ReadFile(file_path):
             text += page.extract_text()
         return text
 def CreateCsv(file_path, data):
-    series = pd.Series(data)
-    series.to_csv(file_path, index=False, header=False)
+    dataframe = pd.DataFrame(data)
+    dataframe.to_csv(file_path, index=False, header=False)
 def ReplaceTermin(text, terminology_dict):
     for key, value in terminology_dict.items():
         text = text.replace(key, value)
